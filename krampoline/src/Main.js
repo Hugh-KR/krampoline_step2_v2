bc963d7836c2f1652d586617debb040e8e6c2a41
sds2317884 <sds2317884@daum.net>
2024-12-10 07:36:51 +0000

    이름 변경

--- krampoline/src/Main.js
+++ krampoline/src/Main.js
@@ -31,7 +31,7 @@ function Main() {
       <header className="App-header">
         
         <div>
-          Hello Krampoline !
+          Hello 창혁 !
         </div>
         <div>
           <button onClick={handleApiOnclick}>API TEST</button><div>{apiTest?"CONNECT":"NOT YET"}</div>
